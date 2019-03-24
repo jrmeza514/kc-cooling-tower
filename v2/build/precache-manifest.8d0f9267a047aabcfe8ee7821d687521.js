ab8ec3aff72f1ebb2d3115be6cea6b544911cb3e
Juan Meza <jrmeza514@gmail.com>
2019-03-24 11:17:20 -0700

    build

---
+++ v2/build/precache-manifest.8d0f9267a047aabcfe8ee7821d687521.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a22b32ba1499c802232",
+    "url": "/static/css/main.edeb6c71.chunk.css"
+  },
+  {
+    "revision": "4a22b32ba1499c802232",
+    "url": "/static/js/main.4a22b32b.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "07b7f484d1ec6b73f27c",
+    "url": "/static/js/2.07b7f484.chunk.js"
+  },
+  {
+    "revision": "95cff60e28e4c4cd8a4128f7aae3412e",
+    "url": "/static/media/baseline-delete-24px.95cff60e.svg"
+  },
+  {
+    "revision": "79b4c2390728069b573becaa513aad04",
+    "url": "/static/media/baseline-pause.79b4c239.svg"
+  },
+  {
+    "revision": "c195ea9bb6271138e6885abeacaaf002",
+    "url": "/static/media/baseline-delete-white-24px.c195ea9b.svg"
+  },
+  {
+    "revision": "8853fa202b8a0a6bbe4ae5722aa18c9a",
+    "url": "/static/media/baseline-play_arrow.8853fa20.svg"
+  },
+  {
+    "revision": "169edf81ac63597a7fed419ee9df8864",
+    "url": "/index.html"
+  }
+];
